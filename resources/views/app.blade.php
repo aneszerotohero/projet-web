80fc190a2f3f9fd2315f94d19777d1645305c627
AnesGzl <mohamedguezouli6@gmail.com>
2026-01-14 20:15:59 +0100

    finale avec icor

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,12 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Projetschool') }}</title>
+    <link rel="icon" type="image/png" href="{{ asset('graduated.png') }}?v=1.0">
+
     @if (class_exists(\Inertia\Inertia::class))
         @inertiaHead
     @endif
 
     @vite(['resources/css/app.css', 'resources/js/app.jsx'])
-    
+
 </head>
 <body>
     @if (class_exists(\Inertia\Inertia::class))
